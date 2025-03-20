ad72872bb936bd45f7a657635234b508d36d57cc
gennhonglin <73550732+gennhonglin@users.noreply.github.com>
2025-03-20 04:03:20 -0700

    Made changes to the form as using useState is a more efficient way to update the data than useRef and using onChange helps update it in real time

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,5 +1,5 @@
 import "./Form.scss";
-import { useRef } from "react";
+import { useState } from "react";
 import axios from "axios";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -7,41 +7,41 @@ import Button from '@mui/material/Button';
 
 export default function Form() {
     //Grabs the input fields data
-    const fNameRef = useRef();
-    const lNameRef = useRef();
-    const numberRef = useRef();
-    const emailRef = useRef();
+    const [formData, setFormData] = useState({
+        fName: "",
+        lName: "",
+        number: "",
+        email: ""
+    });
+    
+     //Updates the state of the input fields
+    const handleChange = (e) => {
+        setFormData({
+            ...formData, [e.target.name]: e.target.value
+        });
+    }
+
+
 
     //submits data to the backend
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        //Groups up the input field data and sends it to the back-end
-        const formDetails = {
-            fName: fNameRef.current.value,
-            lName: lNameRef.current.value,
-            number: numberRef.current.value,
-            email: emailRef.current.value,
-        };
 
-        const phoneNumber = numberRef.current.value;
+        //This should ensure that in the phone number input field that the user only enters numbers and that the number is between 3 and 10 characters long.
         const phoneValidation = /^[0-9]{3,10}$/;
         
-        if(!phoneValidation.test(phoneNumber)) {
+        if(!phoneValidation.test(formData.number)) {
             alert("Please enter a valid phone number.");
-            return;
+            return; 
         }
 
         try {
-            await axios.post("http://localhost:5000/submit", formDetails);
+            await axios.post("http://localhost:5000/submit", formData);
             alert("Form submitted successfully!");
 
             //Resets the input form fields and clears them.
-            fNameRef.current.value = "";
-            lNameRef.current.value = "";
-            numberRef.current.value = "";
-            emailRef.current.value = "";
-
+            setFormData({ fName: "", lName: "", number: "", email: "" });
         } catch (err) {
             console.log(err);
         }
@@ -57,7 +57,7 @@ export default function Form() {
             {/* Input Fields */}
             <div className="form__input">
                 <TextField
-                 id="filled-basic" type="text" label="First Name" name="fName" variant="filled" inputRef={fNameRef} required
+                 id="filled-basic" type="text" label="First Name" name="fName" variant="filled" value={formData.fName} onChange={handleChange} required
                  sx={{
                     input: { color: 'white'},
                     label: { color: 'lightgrey'},
@@ -70,7 +70,7 @@ export default function Form() {
             </div>
             <div className="form__input">
                 <TextField
-                id="filled-basic" type="text" label="Last Name" name="lName" variant="filled" inputRef={lNameRef} required
+                id="filled-basic" type="text" label="Last Name" name="lName" variant="filled" value={formData.lName} onChange={handleChange} required
                 sx={{
                     input: { color: 'white'},
                     label: { color: 'lightgrey'},
@@ -83,7 +83,7 @@ export default function Form() {
             </div>
             <div className="form__input">
                 <TextField
-                id="filled-basic" type="tel" label="Phone Number" name="number" variant="filled" inputRef={numberRef} required
+                id="filled-basic" type="tel" label="Phone Number" name="number" variant="filled" value={formData.number} onChange={handleChange} required
                 sx={{
                     input: { color: 'white'},
                     label: { color: 'lightgrey'},
@@ -96,7 +96,7 @@ export default function Form() {
             </div>
             <div className="form__input">
                 <TextField
-                id="filled-email" type="email" label="Email" name="email" variant="filled" inputRef={emailRef} required
+                id="filled-email" type="email" label="Email" name="email" variant="filled" value={formData.email} onChange={handleChange} required
                 sx={{
                     input: { color: 'white'},
                     label: { color: 'lightgrey'},
